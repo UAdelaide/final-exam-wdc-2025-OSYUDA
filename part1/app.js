cf122bd2fa50b10b80ff81b27443154962f24c0e
OSYUDA <bowenzhu650@gmail.com>
2025-06-20 07:25:27 +0000

    [automatic commit] 2025-06-20 07:25:27 UTC 58.174.5.247

--- part1/app.js
+++ part1/app.js
@@ -2,3 +2,9 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 const PORT = 3000;
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',           // ← 根据你设置写密码
+    database: 'DogWalkService'
+  });
