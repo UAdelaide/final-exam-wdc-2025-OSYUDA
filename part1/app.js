7e074770525a927b8e704021d5de8a68be1f0a07
OSYUDA <bowenzhu650@gmail.com>
2025-06-20 07:42:40 +0000

    [automatic commit] 2025-06-20 07:42:40 UTC 58.174.5.247

--- part1/app.js
+++ part1/app.js
@@ -67,4 +67,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     } catch (err) {
       res.status(500).json({ error: 'Unable to fetch walker summaries' });
     }
+  });// ✅ 第四步：启动服务器
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
   });
