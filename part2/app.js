dd6a580dff41fd6fbd1d20501c68010f37443f38
OSYUDA <bowenzhu650@gmail.com>
2025-06-21 04:31:13 +0000

    [automatic commit] 2025-06-21 04:31:13 UTC 58.174.5.247

--- part2/app.js
+++ part2/app.js
@@ -50,7 +50,9 @@ app.post('/login', (req, res) => {
     // add to direct password comparison, not using hashing
     if (password === user.password_hash) {
         req.session.user = user;
-
+    } else {
+        return res.send('Incorrect password'); 
+      }
         // add to send user to the correct dashboard based on role
         if (user.role === 'owner') {
             return res.redirect('/owner-dashboard.html');
