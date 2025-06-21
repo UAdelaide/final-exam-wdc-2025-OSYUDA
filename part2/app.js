2438db8c4a4f40f25e2d091173e14b2a82821232
OSYUDA <bowenzhu650@gmail.com>
2025-06-21 03:43:09 +0000

    [automatic commit] 2025-06-21 03:43:09 UTC 58.174.5.247

--- part2/app.js
+++ part2/app.js
@@ -53,12 +53,12 @@ app.post('/login', (req, res) => {
 
         // Redirect based on user role
         if (user.role === 'owner') {
-          return res.redirect('/owner-dashboard.html');
-        } else if (user.role === 'walker') {
-          return res.redirect('/walker-dashboard.html');
-        } else {
-          return res.send('Unknown role');
-        }
+            return res.redirect('/owner-dashboard.html');
+          } else if (user.role === 'walker') {
+            return res.redirect('/walker-dashboard.html');
+          } else {
+            return res.send('Unknown role');
+          }
       } else {
         return res.send('Incorrect password');
       }
