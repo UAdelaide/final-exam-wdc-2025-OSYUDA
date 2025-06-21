643985c2139543bb238d20bee2f858cf621d1608
OSYUDA <bowenzhu650@gmail.com>
2025-06-21 04:01:58 +0000

    [automatic commit] 2025-06-21 04:01:58 UTC 58.174.5.247

--- part2/app.js
+++ part2/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-// Middleware (unedited)
+// Middleware (no edit)
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
